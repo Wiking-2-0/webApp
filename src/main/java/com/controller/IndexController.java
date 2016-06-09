a8dfa277903cebeeee8cbb292158f05ee72f94a5
Tropik3572 <alexey3572@gmail.com>
2016-06-09 14:43:09 +0300

    Autowired removed

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.controller;
 
-import com.entity.*;
 import com.mailWork.GmailMail;
-import com.mailWork.YandexMail;
 import com.service.*;
-import com.service.realization.MailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +19,8 @@ public class IndexController {
     private InboxService inboxService;
     @Autowired
     private OutboxService outboxService;
-    @Autowired
-    private GmailMail gmailMail;
+   // @Autowired
+    private GmailMail gmailMail = new GmailMail();
 
     @RequestMapping(value = "/")
     public String getIndex() {
